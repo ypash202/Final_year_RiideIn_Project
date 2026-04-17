0620539586f16eb4418ed2a001b817a947543a29
ypash202 <sherpapasang.np@gmail.com>
2026-04-17 16:38:10 +0545

    feat: create driver citizenship upload page layout

---
+++ app/src/main/java/com/riidein/app/activities/DriverCitizenshipUploadActivity.kt
@@ -0,0 +1,12 @@
+package com.riidein.app.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.riidein.app.R
+
+class DriverCitizenshipUploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_driver_citizenship_upload)
+    }
+}
