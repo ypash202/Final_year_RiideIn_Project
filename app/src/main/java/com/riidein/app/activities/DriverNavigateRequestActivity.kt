9c4fd2f0fca6563b8a0f651dbffc7ef19f37beb2
ypash202 <sherpapasang.np@gmail.com>
2026-04-18 15:29:01 +0545

    feat: add driver navigate layout page

---
+++ app/src/main/java/com/riidein/app/activities/DriverNavigateRequestActivity.kt
@@ -0,0 +1,22 @@
+package com.riidein.app.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.riidein.app.R
+
+class DriverNavigateRequestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_driver_navigate_request)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
